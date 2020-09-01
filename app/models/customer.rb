88494f15328fe3d735640cb0055d6b1ab5a912a5
Michael Evans <michaeljevans113@gmail.com>
2020-08-31 20:49:30 -0600

    Add dependencies

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   validates :first_name, :last_name, presence: true
 
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ApplicationRecord
   validates :unit_price, numericality: true, presence: true
 
   belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items, dependent: :destroy
 end
