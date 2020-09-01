dbcad70a7c08132468fe36e7ffbd7451d80d7752
Michael Evans <michaeljevans113@gmail.com>
2020-08-31 20:03:56 -0600

    Add validations for credit_card_number, credit_card_expiration_date, and result

---
+++ app/models/transaction.rb
@@ -0,0 +1,8 @@
+class Transaction < ApplicationRecord
+  validates :credit_card_number,
+            :credit_card_expiration_date,
+            :result,
+            presence: true
+
+  belongs_to :invoice
+end
