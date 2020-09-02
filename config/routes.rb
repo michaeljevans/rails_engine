3024420e5991e4c0337c041608fb31cb0c8a085f
Michael Evans <michaeljevans113@gmail.com>
2020-09-01 21:36:51 -0600

    Add post route for merchant creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/merchants',     to: 'merchants#index'
-      get '/merchants/:id', to: 'merchants#show'
+      get  '/merchants',     to: 'merchants#index'
+      get  '/merchants/:id', to: 'merchants#show'
+      post '/merchants',     to: 'merchants#create'
     end
   end
 end
