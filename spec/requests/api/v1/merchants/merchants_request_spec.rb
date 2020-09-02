d36ded1c86b85eec77f7cd9f8fd97b9d6309ad6f
Michael Evans <michaeljevans113@gmail.com>
2020-09-01 21:53:21 -0600

    Add tests for updating and destroying a merchant

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe 'Merchants API' do
 
   it 'can create a new merchant' do
     merchant_params = { name: Faker::Company.name }
-    headers = { 'CONTENT_TYPE' => 'application/json' }
+    headers = { 'CONTENT_TYPE': 'application/json' }
 
     post '/api/v1/merchants',
       headers: headers,
@@ -41,5 +41,37 @@ RSpec.describe 'Merchants API' do
     merchant = Merchant.last
 
     expect(merchant.name).to eq(merchant_params[:name])
+  end
+
+  it 'can update an existing merchant' do
+    id = create(:merchant).id
+    name = Merchant.last.name
+    merchant_params = { name: 'Jabroni' }
+    headers = { 'CONTENT_TYPE': 'application/json' }
+
+    patch "/api/v1/merchants/#{id}",
+      headers: headers,
+      params: JSON.generate({ merchant: merchant_params })
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json')
+
+    merchant = Merchant.find_by(id: id)
+
+    expect(merchant.name).to_not eq(name)
+    expect(merchant.name).to eq('Jabroni')
+  end
+
+  it 'can destroy an existing merchant' do
+    merchant = create(:merchant)
+
+    expect(Merchant.count).to eq(1)
+
+    expect{ delete "/api/v1/merchants/#{merchant.id}" }.to change(Merchant, :count).by(-1)
+
+    expect(response).to be_successful
+
+    expect(Merchant.count).to eq(0)
+    expect{ Merchant.find(merchant.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
