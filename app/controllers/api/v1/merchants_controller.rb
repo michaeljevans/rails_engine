a0920fbf85245a21196ebe921ced31b8d0257f3f
Michael Evans <michaeljevans113@gmail.com>
2020-09-01 21:53:58 -0600

    Add update and destroy actions

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,7 +8,15 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def create
-    render json: Merchant.create!(merchant_params)
+    render json: Merchant.create(merchant_params)
+  end
+
+  def update
+    render json: Merchant.update(params[:id], merchant_params)
+  end
+
+  def destroy
+    render json: Merchant.delete(params[:id])
   end
 
   private
