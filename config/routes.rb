26daaab6713cc22d468334d8142806fc4ec3039a
Michael Evans <michaeljevans113@gmail.com>
2020-09-01 23:11:16 -0600

    Use resources instead of hand-rolled routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get    '/merchants',     to: 'merchants#index'
-      post   '/merchants',     to: 'merchants#create'
-      patch  '/merchants/:id', to: 'merchants#update'
-      delete '/merchants/:id', to: 'merchants#destroy'
-      get    '/merchants/:id', to: 'merchants#show'
+      resources :merchants, except: [:new, :edit]
+      resources :items, except: [:new, :edit]
 
-      get    '/items',     to: 'items#index'
-      post   '/items',     to: 'items#create'
-      patch  '/items/:id', to: 'items#update'
-      delete '/items/:id', to: 'items#destroy'
-      get    '/items/:id', to: 'items#show'
+      # get    '/merchants',     to: 'merchants#index'
+      # post   '/merchants',     to: 'merchants#create'
+      # patch  '/merchants/:id', to: 'merchants#update'
+      # delete '/merchants/:id', to: 'merchants#destroy'
+      # get    '/merchants/:id', to: 'merchants#show'
+
+      # get    '/items',     to: 'items#index'
+      # post   '/items',     to: 'items#create'
+      # patch  '/items/:id', to: 'items#update'
+      # delete '/items/:id', to: 'items#destroy'
+      # get    '/items/:id', to: 'items#show'
     end
   end
 end
