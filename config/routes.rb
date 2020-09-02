2fe05a444d8d097cd8b30b876fb1941e6fecf575
Michael Evans <michaeljevans113@gmail.com>
2020-09-01 21:53:43 -0600

    Add routes for updating and deleting a merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get  '/merchants',     to: 'merchants#index'
-      get  '/merchants/:id', to: 'merchants#show'
-      post '/merchants',     to: 'merchants#create'
+      get    '/merchants',     to: 'merchants#index'
+      post   '/merchants',     to: 'merchants#create'
+      patch  '/merchants/:id', to: 'merchants#update'
+      delete '/merchants/:id', to: 'merchants#destroy'
+      get    '/merchants/:id', to: 'merchants#show'
     end
   end
 end
