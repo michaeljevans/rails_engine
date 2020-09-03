053944a5d714185792dd41e47956abedcacd0602
Michael Evans <michaeljevans113@gmail.com>
2020-09-03 05:44:14 -0600

    Add route for finding merchants with the most items sold

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get '/find_all',     to: 'search#index'
         get '/find',         to: 'search#show'
         get '/most_revenue', to: 'revenue#index'
+        get '/most_items',   to: 'items#most_sold'
       end
 
       namespace :items do
