0e151c736a65358ea6425061cb43b97ff4fba898
Michael Evans <michaeljevans113@gmail.com>
2020-09-03 07:41:37 -0600

    Add .total_merchant_revenue()

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -1,11 +1,19 @@
 class RevenueFacade
   def self.revenue_in_interval(dates)
-    start_date = dates[:start]
-    end_date   = dates[:end]
+    first = dates[:start]
+    last  = dates[:end]
     total = Invoice.joins(:invoice_items, :transactions)
-      .where(updated_at: Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day)
+      .where(updated_at: Date.parse(first).beginning_of_day..Date.parse(last).end_of_day)
       .where(status: 'shipped')
       .where("transactions.result='success'")
+      .sum('invoice_items.quantity * invoice_items.unit_price')
+    Revenue.new(total)
+  end
+
+  def self.total_merchant_revenue(merchant_id)
+    total = Merchant.joins(invoices: [:invoice_items, :transactions])
+      .where(id: merchant_id)
+      .where("invoices.status='shipped' AND transactions.result='success'")
       .sum('invoice_items.quantity * invoice_items.unit_price')
     Revenue.new(total)
   end
