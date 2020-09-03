33f9146df562d77edf609a45bd20f29ac95138a8
Michael Evans <michaeljevans113@gmail.com>
2020-09-03 00:53:37 -0600

    Add test for .find_merchant()

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Merchant do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    it '.find_merchant(search_param)' do
+      merchant = Merchant.create(name: 'REI')
+      merchant2 = Merchant.create(name: 'Outdoor Goods')
+      merchant3 = Merchant.create(name: 'We Got The Goodz')
+
+      expect(Merchant.find_merchant(id: merchant.id)).to eq(merchant)
+      expect(Merchant.find_merchant(name: 'dOOr')).to eq(merchant2)
+      expect(Merchant.find_merchant(created_at: merchant.created_at.to_s)).to eq(merchant)
+      expect(Merchant.find_merchant(updated_at: merchant.updated_at.to_s)).to eq(merchant)
+    end
+  end
 end
